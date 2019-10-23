f5da859eec8aea83ec9c2ec057ef2dbe3b68bfe2
taras <stvstudio.com.ua@gmail.com>
2019-10-23 17:36:42 +0300

    Updates

---
+++ precache-manifest.34f71c9ce25aa9b36e1e33d2f565dd46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6db0d3d33877cc1ba023a6c6387abc",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "8073a02d1f593624d080",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.77a45eba.chunk.css"
+  },
+  {
+    "revision": "60640ab0f9eb043c51fe",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.b766c344.chunk.js"
+  },
+  {
+    "revision": "8073a02d1f593624d080",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.adc76d2e.chunk.js"
+  },
+  {
+    "revision": "3416de80b872f582c7c2",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.cfe2b7b1.js"
+  }
+]);
